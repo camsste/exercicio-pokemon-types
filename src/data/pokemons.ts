b86c203cc7ed7f6bd05040eeade5d1bcae0cdc76
Andressa-Limas <agfl@discente.ifpe.edu.br>
2024-10-17 13:45:42 -0300

    update

--- src/data/pokemons.ts
+++ src/data/pokemons.ts
@@ -2,108 +2,147 @@ import { Pokemon } from '../models/pokemonModel';
 
 const pokemons: Pokemon[] = [
     {
-        name: "pikachu",
-        types: ["electric"],
-        weaknesses: ["ground"],
-        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png"
-    },
-    {
-        name: "charizard",
-        types: ["fire", "flying"],
-        weaknesses: ["water", "electric", "rock"],
-        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png"
-    },
-    {
+        id: 1,
         name: "bulbasaur",
         types: ["grass", "poison"],
         weaknesses: ["fire", "psychic", "flying", "ice"],
         image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"
     },
     {
+        id: 2,
+        name: "ivysaur",
+        types: ["grass", "poison"],
+        weaknesses: ["fire", "psychic", "flying", "ice"],
+        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/002.png"
+    },
+    {
+        id: 3,
+        name: "venusaur",
+        types: ["grass", "poison"],
+        weaknesses: ["fire", "psychic", "flying", "ice"],
+        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/003.png"
+    },
+    {
+        id: 4,
+        name: "charmander",
+        types: ["fire"],
+        weaknesses: ["water", "ground", "rock"],
+        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png"
+    },
+    {
+        id: 5,
+        name: "charmeleon",
+        types: ["fire"],
+        weaknesses: ["water", "ground", "rock"],
+        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/005.png"
+    },
+    {
+        id: 6,
+        name: "charizard",
+        types: ["fire", "flying"],
+        weaknesses: ["water", "electric", "rock"],
+        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/006.png"
+    },
+    {
+        id: 7,
         name: "squirtle",
         types: ["water"],
         weaknesses: ["electric", "grass"],
         image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png"
     },
     {
-        name: "mewtwo",
-        types: ["psychic"],
-        weaknesses: ["bug", "ghost", "dark"],
-        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/150.png"
+        id: 8,
+        name: "wartortle",
+        types: ["water"],
+        weaknesses: ["electric", "grass"],
+        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/008.png"
     },
     {
-        name: "gengar",
-        types: ["ghost", "poison"],
-        weaknesses: ["ghost", "psychic", "dark", "ground"],
-        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/094.png"
+        id: 9,
+        name: "blastoise",
+        types: ["water"],
+        weaknesses: ["electric", "grass"],
+        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/009.png"
     },
     {
-        name: "lucario",
-        types: ["fighting", "steel"],
-        weaknesses: ["fire", "fighting", "ground"],
-        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/448.png"
+        id: 25,
+        name: "pikachu",
+        types: ["electric"],
+        weaknesses: ["ground"],
+        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png"
     },
     {
+        id: 39,
         name: "jigglypuff",
         types: ["fairy", "normal"],
         weaknesses: ["steel", "poison"],
         image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/039.png"
     },
     {
-        name: "garchomp",
-        types: ["dragon", "ground"],
-        weaknesses: ["ice", "dragon", "fairy"],
-        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/445.png"
+        id: 94,
+        name: "gengar",
+        types: ["ghost", "poison"],
+        weaknesses: ["ghost", "psychic", "dark", "ground"],
+        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/094.png"
     },
     {
-        name: "lapras",
-        types: ["water", "ice"],
-        weaknesses: ["electric", "grass", "fighting", "rock"],
-        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/131.png"
+        id: 150,
+        name: "mewtwo",
+        types: ["psychic"],
+        weaknesses: ["bug", "ghost", "dark"],
+        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/150.png"
     },
     {
-        name: "aegislash",
-        types: ["steel", "ghost"],
-        weaknesses: ["fire", "ground", "ghost", "dark"],
-        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/681.png"
-    },
-    {
-        name: "salamence",
-        types: ["dragon", "flying"],
-        weaknesses: ["ice", "dragon", "fairy", "rock"],
-        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/373.png"
-    },
-    {
-        name: "scizor",
-        types: ["bug", "steel"],
-        weaknesses: ["fire"],
-        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/212.png"
-    },
-    {
-        name: "gliscor",
-        types: ["ground", "flying"],
-        weaknesses: ["water", "ice"],
-        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/472.png"
-    },
-    {
+        id: 197,
         name: "umbreon",
         types: ["dark"],
         weaknesses: ["fighting", "bug", "fairy"],
         image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/197.png"
     },
     {
-        name: "golem",
-        types: ["rock", "ground"],
-        weaknesses: ["water", "grass", "ice", "fighting", "ground", "steel"],
-        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/076.png"
+        id: 212,
+        name: "scizor",
+        types: ["bug", "steel"],
+        weaknesses: ["fire"],
+        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/212.png"
     },
     {
+        id: 445,
+        name: "garchomp",
+        types: ["dragon", "ground"],
+        weaknesses: ["ice", "dragon", "fairy"],
+        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/445.png"
+    },
+    {
+        id: 448,
+        name: "lucario",
+        types: ["fighting", "steel"],
+        weaknesses: ["fire", "fighting", "ground"],
+        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/448.png"
+    },
+    {
+        id: 472,
+        name: "gliscor",
+        types: ["ground", "flying"],
+        weaknesses: ["water", "ice"],
+        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/472.png"
+    },
+    {
+        id: 478,
         name: "froslass",
         types: ["ice", "ghost"],
         weaknesses: ["fire", "rock", "ghost", "dark", "steel"],
         image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/478.png"
     },
     {
+        id: 681,
+        name: "aegislash",
+        types: ["steel", "ghost"],
+        weaknesses: ["fire", "ground", "ghost", "dark"],
+        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/681.png"
+    },
+    {
+        id: 748,
         name: "toxapex",
         types: ["poison", "water"],
         weaknesses: ["electric", "ground", "psychic"],
--- src/models/pokemonModel.ts
+++ src/models/pokemonModel.ts
@@ -1,5 +1,6 @@
 export interface Pokemon {
-    name: string,
+  id: number;
+  name: string,
   types: string[],
   weaknesses: string[],
   image: string
