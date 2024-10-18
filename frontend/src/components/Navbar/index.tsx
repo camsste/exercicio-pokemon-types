30339cf549eb4e64d27eed40000f9dc9d9fed6cf
Gustavo Rodrigues <gustarodrigues862@gmail.com>
2024-10-18 11:59:48 -0300

    iniciando o front

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,9 @@
+import './index.scss'
+export default function Navbar(){
+    return (
+        <nav className="navbar">
+            <h1 className="page-title">Pokemons Types</h1>
+        </nav>
+    )
+
+}
