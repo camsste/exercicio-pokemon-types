0ebf90ecfe27d92f9eb00e914e210381038aa852
Gustavo Rodrigues <gustarodrigues862@gmail.com>
2024-10-18 15:55:15 -0300

    AJUSTES FRONT

--- frontend/src/components/PokemonList/index.tsx
+++ frontend/src/components/PokemonList/index.tsx
@@ -1,2 +1,48 @@
-//import '../../../../backend/pages/api/pokemon'
+'use client';
+
 import './index.scss';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import './index.scss';
+
+interface Pokemon {
+  name: string;
+  types: string[];
+  image: string;
+}
+
+const PokemonList = () => {
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      try {
+        const response = await axios.get('http://localhost:8080/pokemon/${name}');
+        setPokemons(response.data);
+      } catch (error) {
+        console.error('Error fetching Pokémon data:', error);
+      }
+    };
+
+    fetchPokemons();
+  }, []);
+
+  return (
+    <div className="pokemon-list">
+      {pokemons.length > 0 ? (
+        pokemons.map((pokemon) => (
+          <div key={pokemon.name} className="pokemon-card">
+            <img src={pokemon.image} alt={pokemon.name} />
+            <h3>{pokemon.name}</h3>
+            <p>{pokemon.types.join(', ')}</p>
+          </div>
+        ))
+      ) : (
+        <p>Loading Pokémon...</p>
+      )}
+    </div>
+  );
+};
+
+export default PokemonList;
+
