cb85c3f378be98878edf4b3c482aea89936a92d8
Andressa-Limas <agfl@discente.ifpe.edu.br>
2024-10-21 13:12:46 -0300

    contruindo o front

--- frontend/src/components/PokemonList/index.tsx
+++ frontend/src/components/PokemonList/index.tsx
@@ -2,47 +2,25 @@
 
 import './index.scss';
 import { useEffect, useState } from 'react';
-import axios from 'axios';
-import './index.scss';
+import { Pokemon } from 'C:/Users/andre/exercicio-pokemon-types/backend/src/models/pokemonModel'; // Certifique-se de ajustar o caminho correto
+import { pokemonData } from 'C:/Users/andre/exercicio-pokemon-types/backend/src/data/pokemonsdata';  // Caminho correto para a lista de pokémons
 
-interface Pokemon {
-  name: string;
-  types: string[];
-  image: string;
-}
-
-const PokemonList = () => {
-  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+export default function PokemonList() {
+  const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
 
   useEffect(() => {
-    const fetchPokemons = async () => {
-      try {
-        const response = await axios.get('http://localhost:8080/pokemon/${name}');
-        setPokemons(response.data);
-      } catch (error) {
-        console.error('Error fetching Pokémon data:', error);
-      }
-    };
-
-    fetchPokemons();
+    setPokemonList(pokemonData);
   }, []);
 
   return (
-    <div className="pokemon-list">
-      {pokemons.length > 0 ? (
-        pokemons.map((pokemon) => (
-          <div key={pokemon.name} className="pokemon-card">
-            <img src={pokemon.image} alt={pokemon.name} />
-            <h3>{pokemon.name}</h3>
-            <p>{pokemon.types.join(', ')}</p>
-          </div>
-        ))
-      ) : (
-        <p>Loading Pokémon...</p>
-      )}
-    </div>
+    <ul className="pokemon-list">
+      {pokemonList.map((pokemon) => (
+        <li key={pokemon.id} className="pokemon-item">
+          <img src={pokemon.image} alt={pokemon.name} className="pokemon-image" />
+          <h3>{pokemon.name}</h3>
+          <p>Types: {pokemon.types.join(', ')}</p>
+        </li>
+      ))}
+    </ul>
   );
-};
-
-export default PokemonList;
-
+}
